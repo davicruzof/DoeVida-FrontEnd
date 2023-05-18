d92aab95f94b8519dc64e6892abed20394b95366
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2023-05-18 03:27:49 -0300

    configure translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { useTranslation } from "react-i18next";
+
 function App() {
-  return <p className="read-the-docs">Hello World</p>;
+  const { t, i18n } = useTranslation();
+
+  return (
+    <div>
+      <p className="read-the-docs">{t("welcome")}</p>
+      <button onClick={() => i18n.changeLanguage("en")}>Mudar para EN</button>
+    </div>
+  );
 }
 
 export default App;
