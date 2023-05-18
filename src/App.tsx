5b085a79354909a1ae5a5a476e88df605b74cf68
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2023-05-18 03:44:30 -0300

    configure router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useTranslation } from "react-i18next";
+import Navigation from "./routes";
 
 function App() {
-  const { t, i18n } = useTranslation();
-
-  return (
-    <div>
-      <p className="read-the-docs">{t("welcome")}</p>
-      <button onClick={() => i18n.changeLanguage("en")}>Mudar para EN</button>
-    </div>
-  );
+  return <Navigation />;
 }
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import { Profile } from "../pages/Profile";
+
+const Navigation = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+};
+
+export default Navigation;
