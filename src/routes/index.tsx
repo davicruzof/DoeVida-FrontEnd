a5d62083be99a9b15d4e76f4341e392eef373212
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2023-05-18 15:25:38 -0300

    update actions

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home";
-import { Profile } from "@/pages/Profile";
-import { SignIn } from "@/pages/SignIn";
+import SignIn from "@/pages/SignIn";
 
 const Navigation = () => {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
